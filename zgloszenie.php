e9fd8af8e0c76074f1a79ca6299a84f7546038a6
NorthmanPL <72551355+NorthmanPL@users.noreply.github.com>
2021-10-19 18:15:35 +0200

    Add files via upload

--- zgloszenie.php
+++ zgloszenie.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 <html lang="pl">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wędkowanie - moje hobby</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Wedkowanie to moje hobby</title>
 </head>
 <body>
-    <?php
-    $lowisko = $_POST['lowisko'];
-    $data = $_POST['data'];
-    $sedzia = $_POST['sedzia'];
-
-    $db = new mysqli('localhost', 'root', '', 'wedkarstwo')
-    $sql = "INSERT INTO `zawody_wedkarskie`(`Karty_wedkarskie_id`, `Lowisko_id`, `data_zawodow`, `sedzia`) VALUES (NULL, `2`, `$lowisko`, `$data` ,`$sedzia`)";
-    $db->querry($sql);
-    $db->close();
-    ?>
+<?php
+$łowisko = $_POST['łowisko'] ; 
+$data = $_POST['data'] ;
+$sędzia = $_POST['sędzia'] ;
+$db = new mysql('localhost', 'root', '', 'wedkarstwo')
+ $sql = "INSERT INTO `zawody_wedkarskie`(`Karty_wedkarskie_id`, `Lowisko_id`, `data_zawodow`, `sedzia`) VALUES (NULL, `2`, `$lowisko`, `$data` ,`$sedzia`)";
+$db->querry($sql);
+$db->close();
+?>
 </body>
 </html>
